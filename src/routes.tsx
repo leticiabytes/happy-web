4d7aaed97a2ee1e069cb0634a6e65870ac902c2e
Letícia <lvieira268@gmail.com>
2020-12-18 20:15:34 -0300

    init project
    Signed-off-by: Letícia <lvieira268@gmail.com>

---
+++ src/routes.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import LandingPage from './pages/LandingPage';
+import OrphanagesMap from './pages/OrphanagesMap';
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
+
+
+const Routes: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={LandingPage}></Route>
+        <Route path="/app" component={OrphanagesMap}></Route>
+
+        <Route path="/orphanages/create" component={CreateOrphanage}></Route>
+        <Route path="/orphanages/:id" component={Orphanage}></Route>
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Routes;
